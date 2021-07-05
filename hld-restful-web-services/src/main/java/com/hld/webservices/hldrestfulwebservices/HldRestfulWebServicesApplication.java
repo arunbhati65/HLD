41b2f60b733193f0043a6b87a7bc4dc161d83cb7
arun.bhati <arun.bhati@servicenow.com>
2021-07-05 18:30:01 +0530

    Initial commit

---
+++ hld-restful-web-services/src/main/java/com/hld/webservices/hldrestfulwebservices/HldRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.hld.webservices.hldrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HldRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HldRestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ hld-restful-web-services/src/test/java/com/hld/webservices/hldrestfulwebservices/HldRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hld.webservices.hldrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HldRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
