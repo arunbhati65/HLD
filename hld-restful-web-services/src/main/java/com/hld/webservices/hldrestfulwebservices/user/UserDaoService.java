1e0ea793de99966f8c3c0c583e1988244cd5f739
arun.bhati <arun.bhati@servicenow.com>
2021-09-16 23:58:07 +0530

    Validation APIs,Genreic Execption handling, GET,POST,DELETE API

--- hld-restful-web-services/src/main/java/com/hld/webservices/hldrestfulwebservices/user/UserDaoService.java
+++ hld-restful-web-services/src/main/java/com/hld/webservices/hldrestfulwebservices/user/UserDaoService.java
@@ -2,6 +2,7 @@ package com.hld.webservices.hldrestfulwebservices.user;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -36,6 +37,18 @@ public class UserDaoService {
 		}
 		users.add(user);
 		return user;
+	}
+
+	public User delete(int id) {
+		Iterator<User> iterator=users.iterator();
+		while (iterator.hasNext()){
+			User user=iterator.next();
+			if(user.getId() == id){
+				iterator.remove();
+				return user;
+			}
+		}
+		return null;
 	}
 
 }
--- hld-restful-web-services/src/main/java/com/hld/webservices/hldrestfulwebservices/user/UserResource.java
+++ hld-restful-web-services/src/main/java/com/hld/webservices/hldrestfulwebservices/user/UserResource.java
@@ -1,13 +1,21 @@
 package com.hld.webservices.hldrestfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.hld.webservices.hldrestfulwebservices.exception.UserNotFoundException;
 
 @RestController
 public class UserResource {
@@ -21,12 +29,25 @@ public class UserResource {
 
 	@GetMapping("/user/{id}")
 	public User retrieveOneUser(@PathVariable int id) {
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if (user == null) {
+			throw new UserNotFoundException("id-" + id);
+		}
+		return user;
 	}
 
 	@PostMapping("/user")
-	public User createUser(@RequestBody User user) {
-		return service.save(user);
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+		User savedUser = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 
+	@DeleteMapping("/user/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.delete(id);
+		if (user == null) {
+			throw new UserNotFoundException("id-" + id);
+		}
+	}
 }
