a2531ce35c540a3de3e0fc8a273b58d6e4e5e7fc
Arun Bhati <38494947+arunbhati65@users.noreply.github.com>
2022-02-21 23:43:48 +0530

    Currency Conversion and Currency Exchange microservices integrated using rest template

--- currency-conversion-service/src/main/java/com/microservice/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/microservice/currencyconversionservice/CurrencyConversionController.java
@@ -1,10 +1,12 @@
 package com.microservice.currencyconversionservice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
@@ -13,8 +15,15 @@ public class CurrencyConversionController {
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
 	                                                      @PathVariable String to,
 	                                                      @PathVariable BigDecimal quantity){
-		CurrencyConversion currencyConversion=
-				new CurrencyConversion(1000L,from,to,quantity,BigDecimal.ONE,BigDecimal.ONE);
+		HashMap<String,String> uriVariables=new HashMap<>();
+		uriVariables.put("from",from);
+		uriVariables.put("to",to);
+		CurrencyConversion currencyConversion=new RestTemplate()
+				.getForEntity("http://localhost:8001/currency-exchange/from/{from}/to/{to}"
+						,CurrencyConversion.class,uriVariables).getBody();
+		currencyConversion.setQuantity(quantity);
+		BigDecimal amountTotal=quantity.multiply(currencyConversion.getConversionMultiple());
+		currencyConversion.setTotalCalculatedAmount(amountTotal);
 		return currencyConversion;
 	}
 }
