9ee77de4c217b43532af248c686455befd2fed3f
Arun Bhati <38494947+arunbhati65@users.noreply.github.com>
2022-02-21 22:43:50 +0530

    h2 data populated

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into currency_exchange (id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','INR',65,'');
+    insert into currency_exchange (id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'USA','INA',25,'');
