e413267483eec1824da0e55c7df5085ea8a794fe
Arun Bhati <38494947+arunbhati65@users.noreply.github.com>
2022-02-22 03:13:10 +0530

    registered microseerevices with naming server eureka netflix

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 insert into currency_exchange (id,currency_from,currency_to,conversion_multiple,environment)
-values(10001,'USD','INR',65,'');
+values(10001,'usd','inr',65,'');
     insert into currency_exchange (id,currency_from,currency_to,conversion_multiple,environment)
-values(10002,'USA','INA',25,'');
+values(10002,'usa','ina',25,'');
