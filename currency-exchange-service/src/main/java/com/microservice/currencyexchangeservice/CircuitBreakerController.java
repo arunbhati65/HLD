7d0021c5345f9090887a693d3b0597785be842d9
Arun Bhati <38494947+arunbhati65@users.noreply.github.com>
2022-02-24 23:28:20 +0530

    bulkhead,ratelimiter,circuitbreaker

--- currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/CircuitBreakerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -16,8 +19,11 @@ public class CircuitBreakerController {
 	private Logger fLogger= LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("/sample-api")
-	@Retry(name = "sample-apii",fallbackMethod = "hardcodedResponse")// 3 times by default
-	public String sampleApi(){
+	//@Retry(name = "sample-apii",fallbackMethod = "hardcodedResponse")// 3 times by default
+	//@CircuitBreaker(name = "default",fallbackMethod = "hardcodedResponse")//closed->open->half_open->closed
+	//@RateLimiter(name="default")
+	@Bulkhead(name="default")//max concurrent calls allowed
+	public String sampleApi(){                                            //              half_open ->open
 		fLogger.info("Sample API Call Received");
 		forEntity=new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",String.class);
 		return forEntity.getBody();
